80bdfd8e2f691ea33de1433a1a7dff31b0d2cbc2
NEULiee <asdfz888@naver.com>
2021-01-28 16:23:34 +0900

    .

--- LiveCapturingApp/Pods/Target Support Files/Pods-LiveCapturingApp-LiveCapturingAppUITests/Pods-LiveCapturingApp-LiveCapturingAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LiveCapturingApp_LiveCapturingAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LiveCapturingApp_LiveCapturingAppUITests
-@end
--- LiveCapturingApp/Pods/Target Support Files/Pods-LiveCapturingAppTests/Pods-LiveCapturingAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LiveCapturingAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_LiveCapturingAppTests
-@end
